95093434356759725e1470a437d280666b4f361a
asmwriter <ganeshece1317@gmail.com>
2023-02-26 14:20:20 -0500

    Trying quad tree (serial)

--- src/seq-simulator.cpp
+++ src/seq-simulator.cpp
@@ -12,11 +12,71 @@
 const int QuadTreeLeafSize = 8;
 class SequentialNBodySimulator : public INBodySimulator {
 public:
+
+  void helper(std::unique_ptr<QuadTreeNode> parentNode, std::vector<Particle> &particles, Vec2 bmin, Vec2 bmax){
+    std::cout<<"QuadTreeLeafSize = "<< QuadTreeLeafSize <<", particles size = "<<particles.size()<<std::endl;
+    std::cout<<"Bbox X min: "<<bmin.x<<", max: "<<bmax.x<<std::endl;
+    std::cout<<"Bbox Y min: "<<bmin.y<<", max: "<<bmax.y<<std::endl;
+    //Check if the number of particles is more than QuadTreeLeafSize
+    if(particles.size() <= QuadTreeLeafSize){
+      //Then this is the leaf node.
+      parentNode->isLeaf = 1;
+      return;
+    }
+    else{
+      parentNode->isLeaf = 0;
+      //Insert children nodes
+      Vec2 pivot = (bmin + bmax) * 0.5f;
+      Vec2 size = (bmax - bmin) * 0.5f;
+
+      Vec2 topLeftBmin = Vec2(bmin.x, pivot.y);
+      Vec2 topLeftBmax = Vec2(pivot.x, bmax.y);
+      Vec2 topRightBmin = Vec2(pivot.x, pivot.y);
+      Vec2 topRightBmax = Vec2(bmax.x, bmax.y);
+      Vec2 botLeftBmin = Vec2(bmin.x, bmin.y);
+      Vec2 botLeftBmax = Vec2(pivot.x, pivot.y);
+      Vec2 botRightBmin = Vec2(pivot.x, bmin.y);
+      Vec2 botRightBmax = Vec2(bmax.x, pivot.y);
+
+      std::vector<Particle> topLeftQ, topRightQ, botLeftQ, botRightQ;
+      for(auto &particle: particles){
+
+        if(particle.x > topLeftBmin.x && particle.x < topLeftBmax.x &&
+             particle.y > topLeftBmin.y && particle.y < topLeftBmax.y){
+              topLeftQ.push_back(particle);
+        }
+        if(particle.x > topRightBmin.x && particle.x < topRightBmax.x &&
+             particle.y > topRightBmin.y && particle.y < topRightBmax.y){
+              topRightQ.push_back(particle);
+        }
+        if(particle.x > botLeftBmin.x && particle.x < botLeftBmax.x &&
+             particle.y > botLeftBmin.y && particle.y < botLeftBmax.y){
+              botLeftQ.push_back(particle);
+        }
+        if(particle.x > botRightBmin.x && particle.x < botRightBmax.x &&
+             particle.y > botRightBmin.y && particle.y < botRightBmax.y){
+              botRightQ.push_back(particle);
+        }
+      }
+      std::unique_ptr<QuadTreeNode> topLeftNode     = createQuadTreeNode(topLeftQ, topLeftBmin, topLeftBmax);
+      helper(topLeftNode, topLeftQ, topLeftBmin, topLeftBmax);
+      std::unique_ptr<QuadTreeNode> topRightNode    = createQuadTreeNode(topRightQ, topRightBmin, topRightBmax);
+      helper(topRightNode, topRightQ, topRightBmin, topRightBmax);
+      std::unique_ptr<QuadTreeNode> bottomLeftNode  = createQuadTreeNode(botLeftQ, botLeftBmin, botLeftBmax);
+      helper(bottomLeftNode, botLeftQ, botLeftBmin, botLeftBmax);
+      std::unique_ptr<QuadTreeNode> bottomRightNode = createQuadTreeNode(botRightQ, botRightBmin, botRightBmax);
+      helper(bottomRightNode, botRightQ, botRightBmin, botRightBmax);
+    }
+  }
+
   std::unique_ptr<QuadTreeNode> buildQuadTree(std::vector<Particle> &particles,
                                               Vec2 bmin, Vec2 bmax) {
     // TODO: implement a function that builds and returns a quadtree containing
     // particles.
-    return nullptr;
+    auto root = std::make_unique<QuadTreeNode>();
+    root.particles.push_back(particles);
+
+    return root;
   }
   virtual std::unique_ptr<AccelerationStructure>
   buildAccelerationStructure(std::vector<Particle> &particles) {
